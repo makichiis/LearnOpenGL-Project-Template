26fcefc7cd82377be520228eec56823186be5802
Sarah Evans <sevans.mailing@gmail.com>
2024-05-25 14:09:06 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <cstdlib>
+
+/**
+ * @brief Entry point function.
+ * @param argc The number of arguments in this program, including the program path.
+ * @param argv The arguments passed to this program.
+*/
+int main(int argc, const char** argv) {
+    std::cout << "Hello, world!\n";
+    return EXIT_SUCCESS;
+}
